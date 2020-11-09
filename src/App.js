aaa5435c837e9d325c77df34dca0c6afda5027ae
huynq1 <internship.hpa@lifetimetech.vn>
2020-11-09 17:09:53 +0700

    udpate router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
+import routes from "router";
+import Router from "components/Router";
 
 const App = () => {
     return (
         <div className="wrapper">
             <Suspense fallback={<div>Loading...</div>}>
-                <BrowserRouter></BrowserRouter>
+                <BrowserRouter>
+                    <Switch>
+                        {routes.map((route, i) => (
+                            <Router key={i} {...route}></Router>
+                        ))}
+                    </Switch>
+                </BrowserRouter>
             </Suspense>
         </div>
     );
--- src/router.js
+++ src/router.js
@@ -4,16 +4,11 @@ const routes = [
     {
         exact: true,
         path: "/photos",
-        name: "photos",
         component: React.lazy(() => import("./features/Photo")),
         layout: "Default",
-        meta: {
-            hiddenBanner: true,
-        },
     },
     {
         path: "/login",
-        name: "login",
         component: React.lazy(() => import("./features/Login")),
         layout: "Auth",
     },
