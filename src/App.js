28b908afb6134665657b41ac85b4e1692d5cd6d3
huynt1 <internship.hpa@lifetimetech.vn>
2020-11-12 00:27:08 +0700

    update auth firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
+import firebase from "firebase";
 import {
   BrowserRouter as Router,
   Route,
@@ -7,7 +8,28 @@ import {
 } from "react-router-dom";
 import routes from "router";
 
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_APP_DOMAIN,
+};
+firebase.initializeApp(config);
+
 const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged(async (user) => {
+        if (!user) {
+          console.log("Not sign in");
+          return;
+        }
+        const token = await user.getIdToken();
+        window.localStorage.setItem("firebase-token", token);
+        setIsSignedIn(!!user);
+      });
+    return () => unregisterAuthObserver();
+  }, []);
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Router>
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import InputField from "custom-fields/InputField/InputField";
+import { Form, Formik, FastField } from "formik";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+const uiConfig = {
+  signInFlow: "popup",
+  signInSuccessUrl: "/photos",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+};
 
 const Login = () => {
+  const [initialValues] = useState(() => ({
+    email: "",
+    password: "",
+  }));
+  const handleSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div className="auth-content shadow bg-white rounded p-4">
+    <div className="auth-content shadow bg-white rounded p-4 text-center">
       <h1 className="font-18">Welcome Back To Pexels</h1>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      <p>OR</p>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {(formikProps) => {
+          return (
+            <Form>
+              <FastField
+                name="email"
+                component={InputField}
+                placeholder="Email"
+              />
+              <FastField
+                name="password"
+                component={InputField}
+                placeholder="Password"
+              />
+              <button className="btn btn-success btn-block" type="submit">
+                Login
+              </button>
+            </Form>
+          );
+        }}
+      </Formik>
+      <p className="my-3">Forgot your password?</p>
     </div>
   );
 };
